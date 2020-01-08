7a80126d44f7c3180e0c6c8b7a6b1c27ac88296d
dhruvk <dhruvkapur91@gmail.com>
2020-01-08 18:55:18 +0530

    Update report for parallel collections

--- src/main/scala/montecarlo/parallelcollections/Runner.scala
+++ src/main/scala/montecarlo/parallelcollections/Runner.scala
@@ -30,3 +30,15 @@ object Runner extends App {
   (1 to numberOfPoints).map(raise10To).map(numberOfPoints => (runExperiment(numberOfPoints), numberOfPoints)).foreach(reporter)
 
 }
+
+/*
+Parallel collections report - Very slow compared to sequential collecctions
+info] running montecarlo.parallelcollections.Runner 7
+It took 1.677748 ms for 10 points
+It took 1.061964 ms for 100 points
+It took 0.562431 ms for 1000 points
+It took 3.582251 ms for 10000 points
+It took 30.981812 ms for 100000 points
+It took 332.464702 ms for 1000000 points / runMain 43s
+It took 3252.623377 ms for 10000000 points
+ */
