62c36fc2f8646b983cd61cf3cc631f2fae753080
dhruvk <dhruvkapur91@gmail.com>
2020-01-08 18:45:02 +0530

    Update reports

--- src/main/scala/montecarlo/sequential/Runner.scala
+++ src/main/scala/montecarlo/sequential/Runner.scala
@@ -29,3 +29,16 @@ object Runner extends App {
   (1 to numberOfPoints).map(raise10To).map(numberOfPoints => (runExperiment(numberOfPoints), numberOfPoints)).foreach(reporter)
 
 }
+
+/*
+Sequential run reports on m4large
+sbt:scala-parallel-programs> run 7
+[info] running montecarlo.sequential.Runner 7
+It took 0.050859 ms for 10 points
+It took 0.057532 ms for 100 points
+It took 0.198189 ms for 1000 points
+It took 1.391727 ms for 10000 points
+It took 10.373825 ms for 100000 points
+It took 75.328267 ms for 1000000 points
+It took 1162.63124 ms for 10000000 points
+ */
